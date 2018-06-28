1abaa869bb849536e0e0d7d98d221d87fad8dd5a
Rincker, Miles <Miles.Rincker@genesys.com>
2018-06-28 10:50:15 -0400

    checkpoint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { Injector, NgModule } from '@angular/core';
+import { ButtonModule } from './button/button.module';
+import { ContainerModule } from './container/container.module';
+import { overrideRenderFactory } from '../shadow-dom-renderer';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [ BrowserModule, ButtonModule, ContainerModule ],
+  providers: [overrideRenderFactory()]
 })
-export class AppModule { }
+export class AppModule {
+    constructor() {}
+
+    ngDoBootstrap() {}
+}
